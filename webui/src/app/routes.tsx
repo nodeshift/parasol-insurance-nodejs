144c789b81d57970f3c19fccf84313953f131a5c
Lucas Holmquist <lholmqui@redhat.com>
2025-01-22 11:23:14 -0500

    chore: adding the webui associated with email branch

--- webui/src/app/routes.tsx
+++ webui/src/app/routes.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { Redirect, Route, RouteComponentProps, Switch, useLocation } from 'react-router-dom';
 import { ClaimDetail } from './components/ClaimDetail/ClaimDetail';
 import { ClaimsList } from './components/ClaimsList/ClaimsList';
-// import { EmailGenerate } from './components/EmailGenerate/EmailGenerate';
+import { EmailGenerate } from './components/EmailGenerate/EmailGenerate';
 import { Empty } from './components/Empty/Empty';
 
 
@@ -86,13 +86,13 @@ const routes: AppRouteConfig[] = [
     path: '#',
     title: 'Reports'
   },
-  // {
-  //   component: EmailGenerate,
-  //   exact: true,
-  //   label: 'Email Generate',
-  //   path: '/EmailGenerate',
-  //   title: 'Email Generate'
-  // },
+  {
+    component: EmailGenerate,
+    exact: true,
+    label: 'Email Generate',
+    path: '/EmailGenerate',
+    title: 'Email Generate'
+  },
   {
     component: Empty,
     label: 'Admin',
