dda85aa338178f70921449194eae4a5c76cf46eb
Lucas Holmquist <lholmqui@redhat.com>
2025-01-29 15:21:43 -0500

    chore: update the env names and fix the temperature value

--- app/ai/ai.mjs
+++ app/ai/ai.mjs
@@ -2,10 +2,10 @@ const { ChatOpenAI } = await import("@langchain/openai");
 
 export function getModel(options = {}) {
   return new ChatOpenAI({
-    temperature: options.temperature || process.env.TEMPERATURE || 0.9,
-    openAIApiKey: options.openAIApiKey || process.env.OPEN_AI_AP_KEY || 'EMPTY',
-    modelName: options.modelName || process.env.AI_MODEL_NAME || 'mistral'
+    temperature: options.temperature || Number(process.env.AI_CHAT_MODEL_TEMPERATURE) || 0.9,
+    openAIApiKey: options.openAIApiKey || process.env.OPENAI_API_KEY || 'EMPTY',
+    modelName: options.modelName || process.env.AI_CHAT_MODEL_NAME || 'mistral'
   }, {
-    baseURL: options.baseURL || process.env.AI_BASE_URL || 'http://localhost:8000/v1'
+    baseURL: options.baseURL || process.env.AI_CHAT_BASE_URL || 'http://localhost:8000/v1'
   });
 }
