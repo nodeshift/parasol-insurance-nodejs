72d0143c2d69a0c123a78143db65ed4b892f7a4c
Lucas Holmquist <lholmqui@redhat.com>
2025-02-14 11:22:43 -0500

    chore: update deps

--- app/ai/ai.mjs
+++ app/ai/ai.mjs
@@ -1,11 +1,12 @@
-const { ChatOpenAI } = await import("@langchain/openai");
+import { ChatOpenAI } from '@langchain/openai';
 
 export function getModel(options = {}) {
   return new ChatOpenAI({
     temperature: options.temperature || Number(process.env.AI_CHAT_MODEL_TEMPERATURE) || 0.9,
     openAIApiKey: options.openAIApiKey || process.env.OPENAI_API_KEY || 'EMPTY',
-    modelName: options.modelName || process.env.AI_CHAT_MODEL_NAME || 'mistral'
-  }, {
-    baseURL: options.baseURL || process.env.AI_CHAT_BASE_URL || 'http://localhost:8000/v1'
+    modelName: options.modelName || process.env.AI_CHAT_MODEL_NAME || 'mistral',
+    configuration: {
+      baseURL: options.baseURL || process.env.AI_CHAT_BASE_URL || 'http://localhost:11434/v1'
+    }
   });
 }
